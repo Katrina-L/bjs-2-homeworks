be714e38362cca267eed3465f06db239d8675e3d
Ekaterina L <Ekaterin.Lys@yandex.ru>
2022-11-01 14:01:38 +0300

    changed task.js: resolved #1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,40 @@
-function Student(name, gender, age) {
-    // Ваш код
+"use strict";
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.setSubject();
+  this.addMark();
+  this.getAverage();
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  } else this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  } else this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function() {
+  if (!(this.marks === undefined)) {
+    this.average = this.marks.reduce( (accum, item) => accum + item, 0) / (this.marks.length);
+    // this.average = +(this.marks.reduce( (accum, item) => accum + item, 0) / (this.marks.length)).toFixed(2);
+  }
+  return this.average;
+}
+
+Student.prototype.exclude = function(reason) {
+delete this.subjectName;
+delete this.marks;
+this.excluded = reason;
+}
